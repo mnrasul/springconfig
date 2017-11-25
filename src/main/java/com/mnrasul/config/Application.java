85f16c4d84cd3b57ad62d5fa74fda83329861efb
Nasir Rasul <nasir@mnrasul.com>
2017-11-25 10:32:38 -0700

    fixed configuration

--- src/main/java/com/mnrasul/config/Application.java
+++ src/main/java/com/mnrasul/config/Application.java
@@ -3,6 +3,7 @@ package com.mnrasul.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * @author Nasir Rasul {@literal nasir@mnrasul.com}
@@ -10,6 +11,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableConfigServer
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
